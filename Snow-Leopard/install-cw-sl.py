dd6a14c2d71600e56ab2ace1f2a4d14bf229fb73
A L 3 X   V 3 G A S <officialalexvegas@gmail.com>
2016-03-16 21:14:12 +0100

    Update install-cw-sl.py

--- Snow-Leopard/install-cw-sl.py
+++ Snow-Leopard/install-cw-sl.py
@@ -16,13 +16,17 @@ os.system("cd ../")
 
 os.system("mv CalcWizard " + str(location))
 
+desktop = raw_input("Type the full path to your desktop here: ")
+
+os.system("cd " + str(desktop))
+
 startsh = open("CalcWizard-starter.sh", "w")
 
 startsh = open("CalcWizard-starter.sh", "a")
 
 startsh.write("#!/bin/bash")
 
-startsh.write("cd" + str(location))
+startsh.write("cd " + str(location))
 
 startsh.write("cd CalcWizard/Snow-Leopard/program")
 
@@ -34,3 +38,27 @@ startsh.write("read")
 
 startsh.write("exit")
 
+startsh.close()
+
+print ("Alle files and drectories have been generated!")
+
+print ("The installation has been completed!")
+
+print ("To start CalcWizard Snow Leopard, open a command prompt on your desktop")
+
+print ("and type 'bash start.sh' to start the program!")
+
+exit = raw_input("Do you want to exit?(y/n)")
+
+if exit == "y":
+  print ("The program will exit in 5 seconds.")
+  time.sleep(5)
+  exit()
+if exit == "n":
+  print ("Exiting anyway!")
+  time.sleep(5)
+  exit()
+else:
+  print ("Exiting...")
+  time.sleep(5)
+  exit()
