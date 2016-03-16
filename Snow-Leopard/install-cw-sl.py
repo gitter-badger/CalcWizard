41e2706ac9e529901e37688206b31d4ede604380
A L 3 X   V 3 G A S <officialalexvegas@gmail.com>
2016-03-16 20:24:28 +0100

    Update install-cw-sl.py

--- Snow-Leopard/install-cw-sl.py
+++ Snow-Leopard/install-cw-sl.py
@@ -1,1 +1,12 @@
 #This software is licensed under the GNU GPL v3.0.
+
+import os
+
+import time
+
+print ("This script will install CalcWizard Snow Leopard on your system!")
+
+location = raw_input("Type the full path to the installation directory here: ")
+
+os.system('
+
