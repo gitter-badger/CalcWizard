a1330383793885c7a8c746789f30c5f39438fc46
A L 3 X   V 3 G A S <officialalexvegas@gmail.com>
2016-03-16 21:03:20 +0100

    Update install-cw-sl.py

--- Snow-Leopard/install-cw-sl.py
+++ Snow-Leopard/install-cw-sl.py
@@ -8,5 +8,29 @@ print ("This script will install CalcWizard Snow Leopard on your system!")
 
 location = raw_input("Type the full path to the installation directory here: ")
 
-os.system('
+ktc = ("Press any key to exit the program!")
+
+os.system("cd ../")
+
+os.system("cd ../")
+
+os.system("mv CalcWizard " + str(location))
+
+startsh = open("CalcWizard-starter.sh", "w")
+
+startsh = open("CalcWizard-starter.sh", "a")
+
+startsh.write("#!/bin/bash")
+
+startsh.write("cd" + str(location))
+
+startsh.write("cd CalcWizard/Snow-Leopard/program")
+
+startsh.write("python snowleopard.py")
+
+startsh.write(str(ktc))
+
+startsh.write("read")
+
+startsh.write("exit")
 
