b79aebcabddd9fc9dfbb88aed0ef02cff26776d2
A L 3 X   V 3 G A S <officialalexvegas@gmail.com>
2016-03-17 14:43:24 +0100

    Added files via upload

---
+++ Snow-Leopard/installer/CalcWizard-SnowLeopard-Setup.sh
@@ -0,0 +1,38 @@
+echo "CalcWizard for Linux"
+echo "Welcome to the Snowleopard Release of"
+echo "CalcWizard for Linux!"
+echo "Installing dependemcies..."
+echo "Press any key to continue!"
+read
+echo "Updating package lists..."
+sudo apt-get update
+echo "...done."
+echo "Press any key to continue!"
+read
+echo "Installing Python..."
+sudo apt-get install python
+echo "...done."
+echo "Press any key to continue!"
+read
+echo "Updating package lists..."
+sudo apt-get update
+echo "...done."
+echo "Press any key to continue!"
+read
+echo "Installing Git..."
+sudo apt-get install git
+echo "...done."
+echo "Press any key to continue!"
+read
+echo "All dependencies have been installed!"
+echo "Starting second part of installation!"
+echo "Press any key to continue!"
+read
+echo "Cloning repos to current directory.."
+git clone https://github.com/al3xv3gas/CalcWizard.git
+cd CalcWizard
+cd installerscripts
+python install-cw-sl.py
+echo ("Press any key to exit the program!")
+read
+exit
